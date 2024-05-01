9174fd2eb5ee9a1e86a1a117864d480205f0fac3
salam.omarbagaev <salam.omarbagaev@bpsoftware.net>
2024-05-02 00:42:23 +1200

    - correct tsv

--- src/shared/types/offer.types.ts
+++ src/shared/types/offer.types.ts
@@ -5,11 +5,11 @@ import { User } from './user.types.js';
 
 export type Offer = {
   /** min 10 max 100 */
-  name: string;
+  title: string;
   /** min 20 max 1024 */
   description: string;
   /** e,g, '2024-01-15T09:51:10.735Z' */
-  publicationDate: Date;
+  postDate: Date;
   /** Paris | Cologne | Hamburg | Brussels |Amsterdam | Dusseldorf */
   city: City;
   /** path to the image preview */
@@ -29,7 +29,7 @@ export type Offer = {
   /** min 1 max 10 */
   numberOfGuests: number;
   /** min 100 max 100 000*/
-  rentalCost: number;
+  price: number;
   /** 'Washer' | 'Towels' | 'Fridge' |  'Baby seat' |
    *  'Breakfast' | 'Air conditioning' | 'Laptop friendly workspace',
    *   1 or many
