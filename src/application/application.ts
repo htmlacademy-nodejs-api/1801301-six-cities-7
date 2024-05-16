1191dd5c3406752bb15001b61c995bac7a22bf16
salam.omarbagaev <salam.omarbagaev@bpsoftware.net>
2024-05-16 23:11:52 +1200

    - added config

--- src/application/application.ts
+++ src/application/application.ts
@@ -1,10 +1,15 @@
+import { Config as IConfig } from '../shared/libs/config/index.js';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Application as IApplication } from './application.interface.js';
 
 export class Application implements IApplication {
-  constructor(private readonly logger: Logger) {}
+  constructor(
+    private readonly logger: Logger,
+    private readonly config: IConfig
+  ) {}
 
   public async init() {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
---
+++ src/shared/libs/config/index.ts
@@ -0,0 +1,2 @@
+export { Config } from './config.interface.js';
+export { Config as RestConfig } from './config.js';
